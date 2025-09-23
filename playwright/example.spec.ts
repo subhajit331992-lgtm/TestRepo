90ec95668b05274d0324af62a985c1e7b8037298
Subhajit Chakraborty <subhajit.chakraborty2@thermofisher.com>
2025-09-23 19:20:54 +0530

    UpdatedSpecFile

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('homepage has correct title', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle('Example Domain');
-  console.log("Title Found : " + await page.title()); 
+  console.log("Title Found : " + await page.title());  
 }); 
