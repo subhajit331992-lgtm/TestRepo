dce62cfe059e68a2199305b60a6f53fc511f4fd2
Subhajit Chakraborty <subhajit.chakraborty2@thermofisher.com>
2025-09-25 12:42:30 +0530

    UpdatedParallel

--- playwright/example.spec1.ts
+++ playwright/example.spec1.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('homepage has correct title', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle('Example Domain');
-  console.log(" Parallel 1 Title Found : " + await page.title());  
+  console.log("Parallel Title Found : " + await page.title());
 }); 
